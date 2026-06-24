7289fe706f2af6f18e57b7f8037bbb419b0fd716
fer170203 <162755026+fer170203@users.noreply.github.com>
2026-06-24 17:33:01 -0400

    Programación interruptores

--- Assets/scripts/InteraccionLuz.cs
+++ Assets/scripts/InteraccionLuz.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class InteraccionLuz : MonoBehaviour
 {
+    [Header("Luces del cuarto")]
+    public Light[] luces = new Light[6];
 
-    public Light luz;
     bool enRangoLuz = false;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if (enRangoLuz && Input.GetKeyDown(KeyCode.F))
+        if (enRangoLuz && Input.GetKeyDown(KeyCode.E))
         {
-            luz.enabled = !luz.enabled;
+            ToggleTodasLasLuces();
         }
-        
+    }
+
+    void ToggleTodasLasLuces()
+    {
+        bool algunaEncendida = false;
+
+        foreach (Light luz in luces)
+        {
+            if (luz != null && luz.enabled)
+            {
+                algunaEncendida = true;
+                break;
+            }
+        }
+
+        foreach (Light luz in luces)
+        {
+            if (luz != null)
+                luz.enabled = !algunaEncendida;
+        }
     }
 
     void OnTriggerEnter(Collider other)
