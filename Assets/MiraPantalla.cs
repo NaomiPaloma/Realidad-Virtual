b5b30a8eda31316c1c6173255ddf2e5de09936ec
Naomi Paloma <paloma.jachniuk@davinci.edu.ar>
2026-07-01 15:44:03 -0300

    nose

--- Assets/MiraPantalla.cs
+++ Assets/MiraPantalla.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class MiraPantalla : MonoBehaviour
 {
     [SerializeField] Texture2D texturaMira;
-    [SerializeField] float tamañoMira = 6f;
+    [SerializeField] float tamañoMira = 30f;
 
     void OnGUI()
     {
         // Calcula el centro exacto de la pantalla
-        float xCentro = (Screen.width - tamañoMira) / 2;
-        float yCentro = (Screen.height - tamañoMira) / 2;
+        float xCentro = (Screen.width - tamañoMira) / 10;
+        float yCentro = (Screen.height - tamañoMira) / 10;
 
         // Dibuja la mira en el centro
         if (texturaMira != null)
